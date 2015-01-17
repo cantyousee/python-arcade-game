4e969d8c06d89a4d637591434775cd156dbcb43a
Danny Peck <dannytpeck@gmail.com>
2015-01-17 16:30:27 -0700

    More Changes
    Updated with more sample games

--- maze runner.py
+++ maze runner.py
@@ -2,9 +2,9 @@ import pygame
 
 BLACK  = (   0,   0,   0)
 WHITE  = ( 255, 255, 255)
-BLUE   = (   0,   0, 255)
-GREEN  = (   0, 255,   0)
 RED    = ( 255,   0,   0)
+GREEN  = (   0, 255,   0)
+BLUE   = (   0,   0, 255)
 PURPLE = ( 255,   0, 255)
 
 
